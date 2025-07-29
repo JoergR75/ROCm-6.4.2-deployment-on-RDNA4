fcdeeeb049d858e97a30477c7706ffd6c2f51ec6
JoergR75 <141847198+JoergR75@users.noreply.github.com>
2025-07-29 15:26:45 +0200

    Update script_module_ROCm_642_Ubuntu_22.04-24.04_pytorch_290_v4server.sh

--- script_module_ROCm_642_Ubuntu_22.04-24.04_pytorch_290_v4server.sh
+++ script_module_ROCm_642_Ubuntu_22.04-24.04_pytorch_290_v4server.sh
@@ -202,7 +202,7 @@ source ~/.bashrc
 cd && echo 'import torch
 
 print("PyTorch version:", torch.__version__)
-print("ROCm version:", torch.version.hip if hasattr(torch.version, "hip") else "Not ROCm build")
+print("ROCm version:", torch.version.hip if hasattr(torch.version, 'hip') else "Not ROCm build")
 print("Is ROCm available:", torch.version.hip is not None)
 print("Number of GPUs:", torch.cuda.device_count())
 print("GPU Name:", torch.cuda.get_device_name(0) if torch.cuda.device_count() > 0 else "No GPU detected")
